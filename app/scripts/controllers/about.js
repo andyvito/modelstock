c8bc737337bfa9c43d214d6b245e7a8a38017a29
andyvito <andyvito@hotmail.com>
2016-07-29 13:52:35 +0000

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modelsstockApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the modelsstockApp
+ */
+angular.module('modelsstockApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modelsstockApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the modelsstockApp
+ */
+angular.module('modelsstockApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
